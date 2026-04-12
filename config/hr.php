b1f5fefd3837d7fd8c6015dd6b33d757968e370f
Le Hoang Yen <yenlh.22ds@vku.udn.vn>
2026-04-12 13:08:12 +0700

    FEATURE001|Update logic wifi check

--- config/hr.php
+++ config/hr.php
@@ -2,8 +2,12 @@
 return [
     'allowed_office_ips' => [
         '14.191.112.123', // << THAY BẰNG IP TĨNH CỦA VĂN PHÒNG BẠN
-        '127.0.0.1', 
+        '127.0.0.1',
         '192.168.65.1',   // Cho phép debug ở local
+        '192.168.23.161',
+        '192.168.23.1',
+        '192.168.1.88',
+        '192.168.1.5'
     ],
     // --- THÊM DÒNG NÀY ---
     'allowed_office_ssids' => [
@@ -11,5 +15,8 @@ return [
         'BichCam', // Thêm các tên Wi-Fi khác nếu có
         'VKU-SinhVien',
         'helu',
+        'Candles Cafe',
+        '<redacted>',
+        'NGUYEN THUC'
     ],
 ];
