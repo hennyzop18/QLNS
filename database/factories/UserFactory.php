2e8cafbfbb812318cf618b25989775719ef9ad14
Le Hoang Yen <yenlh.22ds@vku.udn.vn>
2026-05-10 11:36:21 +0700

    Final Fix: Move Faker to require, Force HTTPS, Update Kiosk URL

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,8 +13,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'), // Mật khẩu mặc định là 'password'
             'remember_token' => Str::random(10),
